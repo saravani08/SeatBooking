7426cf113da1d9c451f0178d50c82b1c7bf2a189
Saravani Nemani <saravaninemani@gmail.com>
2022-06-10 11:34:31 +0530

    Update UsersController.java

--- OnlineSeatBooking/OnlineSeatBooking/src/main/java/com/controller/UsersController.java
+++ OnlineSeatBooking/OnlineSeatBooking/src/main/java/com/controller/UsersController.java
@@ -1,15 +1,21 @@
 package com.controller;
+
+
+
+import java.util.Map;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-
+import com.Exception.UserNotFoundException;
 import com.model.OfficeDAO;
 import com.model.Users;
 import com.model.UsersDAO;
 
 
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class UsersController {
 
@@ -23,6 +29,19 @@ public class UsersController {
 		
 		return "Hello User";
 	}
+	@PostMapping("/userlogin")
+	public String userLogin(@RequestBody Map<String, String> user) {
+		String userName = user.getOrDefault("userName", null);
+		String password = user.getOrDefault("password", null);
+		System.out.println("Username: "+userName);
+		Optional<Users> opt=usersDAO.findByUserName(userName);
+		System.out.println("Admin password: "+opt.get().getPassword());
+		if(opt.isPresent() && opt.get().getPassword().equals(password)) {
+			return "user login success";
+		} else {
+			throw new RuntimeException("Incorrect Details");
+		}	
+	}
 	
 	@PostMapping("/registeruser")
 	public String registerUser(@RequestBody Users users) {
@@ -34,16 +53,32 @@ public class UsersController {
 		usersDAO.save(users);
 		return "user details updated";
 	}
-	@DeleteMapping("/deleteuser")
+	@DeleteMapping("/deleteuser/{id}")
 	public String deleteUser(@PathVariable("id") int uId ) {
 		usersDAO.deleteById(uId);
 		return "UserDeleted";
 		
 	}
-	@GetMapping("/findfloor")
+	@GetMapping("/findfloor/{id}")
 	public String findFloor(@PathVariable("id")int floorNo ) {
 		officeDAO.findById(floorNo);
 		return "floorFound";
+	}
+	@GetMapping("/finduser/{id}") 
+	public Optional<Users> getUser(@PathVariable int id) {
+		Optional<Users> opt=usersDAO.findById(id);
+		if(!opt.isPresent()) {
+			throw new UserNotFoundException();
+		}
+		return usersDAO.findById(id);
+	}
+	@GetMapping("/finduser/{userName}") 
+	public Optional<Users> getUser(@PathVariable String userName) {
+		Optional<Users> opt=usersDAO.findByUserName(userName);
+		if(!opt.isPresent()) {
+			throw new UserNotFoundException();
+		}
+		return usersDAO.findByUserName(userName);
 	}
 	
 	
